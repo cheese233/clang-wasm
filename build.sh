a583ac7d49f525217a7f128cce9d208597abb641
cheese233 <76160654+cheese233@users.noreply.github.com>
2024-06-25 19:27:32 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -25,7 +25,7 @@ if [ ! -d $LLVM_NATIVE/ ]; then
         -DLLVM_ENABLE_PROJECTS="lld;clang"
 fi
 cmake --build $LLVM_NATIVE -- llvm-tblgen clang-tblgen
-if [ "$1" == "clangd" ]; then
+if ["$1" == "prepare" ]; then
     CXXFLAGS="-Dwait4=__syscall_wait4 -pthread" \
     LDFLAGS="\
         -s LLD_REPORT_UNDEFINED=1 \
@@ -34,7 +34,7 @@ if [ "$1" == "clangd" ]; then
         -s EXPORTED_RUNTIME_METHODS=FS,ERRNO_CODES,allocateUTF8 \
         -pthread \
         -s MODULARIZE \
-        -s EXPORT_NAME="createClangdModule" \
+        -s EXPORT_NAME="createModule" \
         -s ASYNCIFY \
         -s ENVIRONMENT=web,worker \
     " emcmake cmake -G Ninja \
@@ -54,38 +54,9 @@ if [ "$1" == "clangd" ]; then
         -DLLVM_BUILD_LLVM_DYLIB=OFF \
         -DLLVM_TABLEGEN=$LLVM_NATIVE/bin/llvm-tblgen \
         -DCLANG_TABLEGEN=$LLVM_NATIVE/bin/clang-tblgen
-    
+elif [ "$1" == "clangd" ]; then
     cmake --build $LLVM_BUILD --target clangd
 elif [ "$1" == "clang" ]; then
-    CXXFLAGS="-Dwait4=__syscall_wait4 -pthread" \
-    LDFLAGS="\
-        -s LLD_REPORT_UNDEFINED=1 \
-        -s ALLOW_MEMORY_GROWTH=1 \
-        -s EXPORTED_FUNCTIONS=_main,_free,_malloc \
-        -s EXPORTED_RUNTIME_METHODS=FS,ERRNO_CODES,allocateUTF8 \
-        -pthread \
-        -s MODULARIZE \
-        -s EXPORT_NAME="createClangModule" \
-        -s ASYNCIFY \
-        -s ENVIRONMENT=web,worker \
-    " emcmake cmake -G Ninja \
-        -S $LLVM_SRC/llvm/ \
-        -B $LLVM_BUILD/ \
-        -D CMAKE_C_COMPILER_LAUNCHER=ccache -D CMAKE_CXX_COMPILER_LAUNCHER=ccache \
-        -DCMAKE_BUILD_TYPE=Release \
-        -DLLVM_TARGETS_TO_BUILD="" \
-        -DLLVM_ENABLE_PROJECTS="clang;lld;clang-tools-extra" \
-        -DLLVM_ENABLE_DUMP=OFF \
-        -DLLVM_ENABLE_ASSERTIONS=OFF \
-        -DLLVM_ENABLE_EXPENSIVE_CHECKS=OFF \
-        -DLLVM_ENABLE_BACKTRACES=OFF \
-        -DLLVM_BUILD_TOOLS=OFF \
-        -DLLVM_ENABLE_THREADS=ON \
-        -DLLVM_INCLUDE_TESTS=OFF \
-        -DLLVM_BUILD_LLVM_DYLIB=OFF \
-        -DLLVM_TABLEGEN=$LLVM_NATIVE/bin/llvm-tblgen \
-        -DCLANG_TABLEGEN=$LLVM_NATIVE/bin/clang-tblgen
-    
     cmake --build $LLVM_BUILD --target clang
 fi
 
