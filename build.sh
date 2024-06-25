a692281b581026a38dd896d8817534c2f2810925
cheese233 <76160654+cheese233@users.noreply.github.com>
2024-06-25 10:19:14 +0800

    add clang

--- build.sh
+++ build.sh
@@ -21,42 +21,70 @@ if [ ! -d $LLVM_NATIVE/ ]; then
         -B $LLVM_NATIVE/ \
         -DCMAKE_BUILD_TYPE=Release \
         -DLLVM_TARGETS_TO_BUILD=WebAssembly \
-        -DLLVM_ENABLE_PROJECTS="clang"
+        -DLLVM_ENABLE_PROJECTS="lld;clang"
 fi
 cmake --build $LLVM_NATIVE -- llvm-tblgen clang-tblgen
 
-if [ ! -d $LLVM_BUILD/ ]; then
-    CXXFLAGS="-Dwait4=__syscall_wait4 -pthread" \
-    LDFLAGS="\
-        -s LLD_REPORT_UNDEFINED=1 \
-        -s ALLOW_MEMORY_GROWTH=1 \
-        -s EXPORTED_FUNCTIONS=_main,_free,_malloc \
-        -s EXPORTED_RUNTIME_METHODS=FS,ERRNO_CODES,allocateUTF8 \
-        -pthread \
-        -s MODULARIZE \
-        -s EXPORT_NAME="createClangdModule" \
-        -s ASYNCIFY \
-        -s ENVIRONMENT=web,worker \
-    " emcmake cmake -G Ninja \
-        -S $LLVM_SRC/llvm/ \
-        -B $LLVM_BUILD/ \
-        -DCMAKE_BUILD_TYPE=Release \
-        -DLLVM_TARGETS_TO_BUILD="" \
-        -DLLVM_ENABLE_PROJECTS="clang;clang-tools-extra" \
-        -DLLVM_ENABLE_DUMP=OFF \
-        -DLLVM_ENABLE_ASSERTIONS=OFF \
-        -DLLVM_ENABLE_EXPENSIVE_CHECKS=OFF \
-        -DLLVM_ENABLE_BACKTRACES=OFF \
-        -DLLVM_BUILD_TOOLS=OFF \
-        -DLLVM_ENABLE_THREADS=ON \
-        -DLLVM_INCLUDE_TESTS=OFF \
-        -DLLVM_BUILD_LLVM_DYLIB=OFF \
-        -DLLVM_TABLEGEN=$LLVM_NATIVE/bin/llvm-tblgen \
-        -DCLANG_TABLEGEN=$LLVM_NATIVE/bin/clang-tblgen
-fi
+CXXFLAGS="-Dwait4=__syscall_wait4 -pthread" \
+LDFLAGS="\
+    -s LLD_REPORT_UNDEFINED=1 \
+    -s ALLOW_MEMORY_GROWTH=1 \
+    -s EXPORTED_FUNCTIONS=_main,_free,_malloc \
+    -s EXPORTED_RUNTIME_METHODS=FS,ERRNO_CODES,allocateUTF8 \
+    -pthread \
+    -s MODULARIZE \
+    -s EXPORT_NAME="createClangdModule" \
+    -s ASYNCIFY \
+    -s ENVIRONMENT=web,worker \
+" emcmake cmake -G Ninja \
+    -S $LLVM_SRC/llvm/ \
+    -B $LLVM_BUILD/ \
+    -DCMAKE_BUILD_TYPE=Release \
+    -DLLVM_TARGETS_TO_BUILD="" \
+    -DLLVM_ENABLE_PROJECTS="clang;clang-tools-extra" \
+    -DLLVM_ENABLE_DUMP=OFF \
+    -DLLVM_ENABLE_ASSERTIONS=OFF \
+    -DLLVM_ENABLE_EXPENSIVE_CHECKS=OFF \
+    -DLLVM_ENABLE_BACKTRACES=OFF \
+    -DLLVM_BUILD_TOOLS=OFF \
+    -DLLVM_ENABLE_THREADS=ON \
+    -DLLVM_INCLUDE_TESTS=OFF \
+    -DLLVM_BUILD_LLVM_DYLIB=OFF \
+    -DLLVM_TABLEGEN=$LLVM_NATIVE/bin/llvm-tblgen \
+    -DCLANG_TABLEGEN=$LLVM_NATIVE/bin/clang-tblgen
 
 cmake --build $LLVM_BUILD --target clangd -j12
 
+CXXFLAGS="-Dwait4=__syscall_wait4 -pthread" \
+LDFLAGS="\
+    -s LLD_REPORT_UNDEFINED=1 \
+    -s ALLOW_MEMORY_GROWTH=1 \
+    -s EXPORTED_FUNCTIONS=_main,_free,_malloc \
+    -s EXPORTED_RUNTIME_METHODS=FS,ERRNO_CODES,allocateUTF8 \
+    -pthread \
+    -s MODULARIZE \
+    -s EXPORT_NAME="createClangModule" \
+    -s ASYNCIFY \
+    -s ENVIRONMENT=web,worker \
+" emcmake cmake -G Ninja \
+    -S $LLVM_SRC/llvm/ \
+    -B $LLVM_BUILD/ \
+    -DCMAKE_BUILD_TYPE=Release \
+    -DLLVM_TARGETS_TO_BUILD="" \
+    -DLLVM_ENABLE_PROJECTS="clang;lld" \
+    -DLLVM_ENABLE_DUMP=OFF \
+    -DLLVM_ENABLE_ASSERTIONS=OFF \
+    -DLLVM_ENABLE_EXPENSIVE_CHECKS=OFF \
+    -DLLVM_ENABLE_BACKTRACES=OFF \
+    -DLLVM_BUILD_TOOLS=OFF \
+    -DLLVM_ENABLE_THREADS=ON \
+    -DLLVM_INCLUDE_TESTS=OFF \
+    -DLLVM_BUILD_LLVM_DYLIB=OFF \
+    -DLLVM_TABLEGEN=$LLVM_NATIVE/bin/llvm-tblgen \
+    -DCLANG_TABLEGEN=$LLVM_NATIVE/bin/clang-tblgen
+
+cmake --build $LLVM_BUILD --target clang -j12
+
 rm -rf pkg/dist
 mkdir pkg/dist
-cp build/bin/clangd* pkg/dist
+cp build/bin/* pkg/dist
